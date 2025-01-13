066fe03d8f9ab00d64bd5a9f34620c79c7fc2931
TheRedOwl <74923302+TheRedOwl@users.noreply.github.com>
2025-01-13 21:56:40 +0100

    design update

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,13 @@
 export const midleStyle={
+    display:"flex",
+    flexDirection:"column",
+    alignItems:"center",
     width:'300px',
     position:'absolute',
     top:'50%',
     left:'50%',
     transform:'translate(-50%,-50%)',
-    border:'1px solid gray',
-    padding:'1rem'
+    border:'3px solid var(--color1)',
+    padding:'1rem',
+    borderRadius:"15px",
   }
